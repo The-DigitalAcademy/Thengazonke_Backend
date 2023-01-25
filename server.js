579fb0005ba373ddb4986166577a765a0efbe244
Londotani Daniel <44197763+Ravhugoni@users.noreply.github.com>
2023-01-25 10:42:27 +0200

    livestock

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ const config = {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
+const cors = require('cors');
+
+var corsOptions = {
+    origin:"*"
+}
+app.use(cors(corsOptions));
+
 
 app.use(bodyParser.json())
 app.use(
