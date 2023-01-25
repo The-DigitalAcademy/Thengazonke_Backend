e583cd37b2df2b053e3e53bd29caeb1392c821fa
temapalesa <37109819+temapalesa@users.noreply.github.com>
2023-01-25 16:01:29 +0200

    rate-review done

--- app/controller/rate/rating.js
+++ app/controller/rate/rating.js
@@ -1,34 +1,46 @@
+const { request, response } = require('express');
 const pool = require('../../config/connection');
 
 const handleErr = (err, req, res, next) => {
   res.status(400).send({ error: err.message })
 }
 
-// const createRating = (req, res) => {
-
-//     const {userRaterID, userRateID, transactionID,rate,review} = req.body;
-
-   
-
-//     pool.query('INSERT INTO "public"."Rating"("userRaterID", "userRateID","transactionID","rate","review")VALUES ($1,$2,$3,$4,$5)',[userRaterID, userRateID, transactionID,rate,review], (error, results) => {
-//       if (error) {
-//       }
-//       res.status(201).send(`Rating added`)
-//     }),handleErr
-// }
-
-
 const createRating = (request, response) => {
-    const {serRaterID, userRateID, transactionID,rate,review} = request.body
+    const {userRaterID, userRateID, transactionID,rate,review} = request.body
   
-    pool.query('INSERT INTO "public"."Rating"("userRaterID", "userRateID","transactionID","rate","review")VALUES ($1,$2,$3,$4,$5)', [serRaterID, userRateID, transactionID,rate,review], (error, results) => {
+    pool.query('INSERT INTO "public"."Rating"("userRaterID", "userRateID","transactionID","rate","review")VALUES ($1,$2,$3,$4,$5)', [userRaterID, userRateID, transactionID,rate,review], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(201).send(`Rate added with ID: ${results.rows[0].id}`)
-    }),handleErr
+      response.status(201).send(`Rate added with ID: ${results.rows}`)
+    })
+  }
+
+
+
+const getRating = (request, response) => {
+    pool.query('SELECT * FROM "public"."Rating" ORDER BY "rateID" ASC', (error, results) => {
+      if (err) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const getRatingPerUser = function (request, response) {
+    const id = parseInt(request.params.id)
+    pool.query('SELECT * FROM "public"."Rating" WHERE "userRateID" =$1',[id],(error,results) =>{
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
   }
 
 module.exports = {
-    createRating
+
+    createRating,
+    getRating,
+    getRatingPerUser
+
 }
