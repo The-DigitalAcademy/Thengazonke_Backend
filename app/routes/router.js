4efb951b8fea8517dce20ffdbec177e8ae29e749
Precious-Mokone <115017921+Precious-Mokone@users.noreply.github.com>
2023-02-03 14:30:55 +0200

    pull latest

--- app/routes/router.js
+++ app/routes/router.js
@@ -19,7 +19,7 @@ router.get('/users/getUsers', user.getUsers)
 router.post('/users/createUsers', user.postUsers)
 
 //routes for category
-router.get('/category/getCategory', category.postCategory)
+router.get('/category/getCategory', category.getCategory)
 router.post('/category/createCategory', category.postCategory)
 router.put('/category/updateCategory', category.updateCategory)
 
